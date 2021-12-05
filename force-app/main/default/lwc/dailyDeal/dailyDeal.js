404d70d014984485a718b145d6079669997a19be
Kaya66 <zekezimmermanit@gmail.com>
2021-12-05 17:06:47 -0500

    Daily Deals LWC Logic mostly finished
    Apex, HTML, JS, all working together.
    
    Creates a Lightning carousel that adds / removes items if there is a daily deal active for it.
    
    may need better CSS next.

--- force-app/main/default/lwc/dailyDeal/dailyDeal.js
+++ force-app/main/default/lwc/dailyDeal/dailyDeal.js
@@ -1,34 +1,21 @@
 import { LightningElement, wire } from 'lwc';
 
-import { ShowToastEvent } from 'lightning/platformShowToastEvent';
-
-import DailyDeal from '@salesforce/apex/DailyDeal.getDailyDeal';
 
 
-import NAME_FIELD from '@salesforce/schema/Product2.Name';
-import CODE_FIELD from '@salesforce/schema/Product2.ProductCode';
 import getDailyDeal from '@salesforce/apex/DailyDeal.getDailyDeal';
 
 
 export default class ContactDataTable extends LightningElement {
     data = [];
-    columns = [
-        {label: NAME_FIELD.fieldApiName, fieldName: NAME_FIELD.fieldApiName},
-        {label: CODE_FIELD.fieldApiName, fieldName: CODE_FIELD.fieldApiName}
-    ];
-
-    @wire(getDailyDeal)
-    populateTableData({error, data}) {
-        if(data) {
+    @wire(getDailyDeal) dailyDeal;
+    populateData({error, data}){
+        if(data){
             this.data = data;
-        } else if(error) {
-            const toastEvent = new ShowToastEvent({
-                title: 'Error',
-                message: error,
-                variant: 'error'
-            });
-
-            this.dispatchEvent(toastEvent);
         }
     }
 }
+
+
+
+
+
