8aee1a155f949a6342b9759580039ea3c9d0d626
Kalavast <Mjanosik87@gmail.com>
2021-12-08 18:26:15 -0500

    Added to LWC and prepr for test classes and better apex class
    Haveing Difficulty with the Package still

--- force-app/main/default/lwc/loyalty/loyalty.js
+++ force-app/main/default/lwc/loyalty/loyalty.js
@@ -4,12 +4,12 @@ import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 import Object_NAME from '@salesforce/schema/Loyalty_Customer__c';
 import Loyalty_Customer from '@salesforce/schema/Loyalty_Customer__c.Account__c';
-//import Loyalty_Customer_Account from '@salesforce/schema/Loyalty_Customer__c.   Account__c';
+import Loyalty_Customer_Date from '@salesforce/schema/Loyalty_Customer__c.Date_of_Birth__c';
 //import Loyalty_Customer from '@salesforce/schema/Loyalty_Customer__c.   Loyalty_Customer__c';
 export default class Loyalty extends LightningElement {
     @api objectName = Object_NAME;
     
-    myFields = [Loyalty_Customer];
+    myFields = [Loyalty_Customer, Loyalty_Customer_Date];
 
     handleCaseCreation() {
         const toastEvent = new ShowToastEvent({
