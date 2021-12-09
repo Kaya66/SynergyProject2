61a92ec8d373e80f616516ea8e1fa1e5b3a4ef9c
kelseylinton <Kelseylinton@yahoo.com>
2021-12-09 11:21:18 -0500

    moved image files to static file
    all image files were uploaded to a static resource file in my salesforce org. Getting the images working on lwc still isnt working

--- force-app/main/default/lwc/categoryComponent/categoryComponent.js
+++ force-app/main/default/lwc/categoryComponent/categoryComponent.js
@@ -1,5 +1,11 @@
 import { LightningElement } from 'lwc';
+import image from '@salesforce/resourceURL/static';
 
 export default class CategoryComponent extends LightningElement {
-    
+    man = image + '/ManIcon.jpg';
+    woman = image + '/womanIcon.png';
+    baby = image + '/babyIcon.jpg';
+    dog = image + '/dogIcon.png';
+    tech = image + '/techIcon.png';
+    furniture = image + '/furnitureIcon.png';
 }
